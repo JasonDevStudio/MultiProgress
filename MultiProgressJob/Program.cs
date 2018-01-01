7940108ec378dd2082083e2f2c200a5ac8b277f5
尧杰 <jiedev@163.com>
2018-01-01 12:07:17 +0800

    updated

---
+++ MultiProgressJob/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiProgressJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args != null && args.Length > 0)
+            {
+                decimal.TryParse(args.FirstOrDefault(), out var number);
+                Console.WriteLine(number);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ MultiProgressServer/Form1.cs
@@ -0,0 +1,32 @@
+﻿using DevExpress.Utils.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiProgressServer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SimpleButton1_Click(object sender, EventArgs e)
+        {
+            decimal.TryParse(this.textEdit1.EditValue.ToString(), out var number);
+
+            for (int i = 0; i < number; i++)
+            {
+                
+            }
+
+        }
+    }
+}
